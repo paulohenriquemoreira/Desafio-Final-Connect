373779f44e0fcf688f53fa03db226e60ab7ef5c1
paulohenriquemoreira <paulo.henrick.moreira@hotmail.com>
2026-02-05 23:17:23 -0300

    Criado tela Perfil e responsividade até notebook

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -5,6 +5,6 @@ import React from 'react'
 
 export default function Inicio() {
   return (
-    <div>Inicio</div>
+    <div>Sou o inicio</div>
   )
 }
--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,11 +1,33 @@
 
 import FotoDePerfil from "../../assets/FotoPerfil.png"
+import Styles from "./Perfil.module.scss"
 
 import React from 'react'
 
 export default function Perfil() {
   return (
-    <div>Perfil</div>
+    <section>
+      <article className={Styles.SectionPerfil}>
+        <img src={FotoDePerfil} alt="Foto de Perfil" className={Styles.FotoPerfil} />
+        <div className={Styles.DescricaoPerfil} >
+          <h1>Paulo Henrique Moreira</h1>
+          <h4>Voluntário Ativo</h4>
+          <p className={Styles.TextoDescricao}>Apaixonado por fazer a diferença na comunidade. Acredito que pequenas ações podem 
+            transformar vidas e estou sempre em busca de novas oportunidades para ajudar.</p>
+          <address className={Styles.Endereco}>
+              <p className={Styles.EnderecoItem}>Osasco, SP</p>
+              <p className={Styles.EnderecoItem}>paulo.henrick.moreira@hotmail.com</p>
+              <p className={Styles.EnderecoItem}> Membro desde setembro de 2025</p>
+          </address>
+          <ul className={Styles.ListTags}>
+            <li className={Styles.ListTagsItens}>Educação</li>
+            <li className={Styles.ListTagsItens}>Meio Ambiente</li>
+            <li className={Styles.ListTagsItens}>Assistência Social</li>
+            <li className={Styles.ListTagsItens}>Design</li>
+          </ul>
+        </div>
+      </article>
+    </section>
   )
 }
 
